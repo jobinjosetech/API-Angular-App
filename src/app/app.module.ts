203f1d8c78011b2fb9b14f001ccd483f8baee780
jobin <jobinjose.in@gmail.com>
2022-11-29 09:59:21 +0530

    api link fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ const myRoutes:Routes = [
     AppComponent,
     PassengerAppComponent,
     NavbarComponent,
-    UsPublicDataComponent
+    UsPublicDataComponent,
   ],
   imports: [
     BrowserModule,
